974a177135895f0e0cf41d8aae5abb0bcbf4bc8d
gkaur505 <141872400+gkaur505@users.noreply.github.com>
2023-08-09 16:08:57 -0400

    "update"

---
+++ data.js
@@ -0,0 +1,28 @@
+window.reviewData = [
+
+    {
+        date: "2023/08/01",
+        review: "Love the given explanation for the badminton.",
+        rate: "4/5"
+    },
+    {
+        date: "2023/08/01",
+        review: "Still remember, had a conflict with my sister while playing this game.",
+        rate: "2/5"
+    },
+    {
+        date: "2023/07/02",
+        review: "Love this game.",
+        rate: "5/5"
+    },
+    {
+        date: "2023/07/31",
+        review: "Best game ever.",
+        rate: "5/5"
+    },
+    {
+        date: "2023/08/09",
+        review: "Lot of childhood memories connected to this lovely game.",
+        rate: "4/5"
+    }
+];
